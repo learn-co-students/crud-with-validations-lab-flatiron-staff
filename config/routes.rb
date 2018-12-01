fa631ec71eb471ff09690fe8de3324ed1e9e726c
github username <github email address>
2018-12-01 16:57:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  resources :songs
+  # get "/songs", to: "songs#index", as: "index"
+
+  # get "/songs/new", to: "songs#new", as: "new_song"
+  # get "/songs/:id", to: "songs#show", as: "song"
+  # post "/songs", to: "songs#create"
 end
